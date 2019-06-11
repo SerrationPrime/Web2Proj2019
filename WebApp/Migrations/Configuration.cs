642d193a27b6cc1bf2bba33debdfa8191c1248ea
AshenKhan <39307368+AshenKhan@users.noreply.github.com>
2019-06-11 21:13:26 +0200

    Ukrasen HTML i dodate cene

--- WebApp/Migrations/Configuration.cs
+++ WebApp/Migrations/Configuration.cs
@@ -49,38 +49,176 @@ namespace WebApp.Migrations
                 manager.Create(role);
             }
 
+            #region InitialElements
+
+            var type1 = new TicketType
+            {
+                Id = 1,
+                Type = "Hourly"
+            };
+            var type2 = new TicketType
+            {
+                Id = 2,
+                Type = "Daily"
+            };
+            var type3 = new TicketType
+            {
+                Id = 3,
+                Type = "Monthly"
+            };
+            var type4 = new TicketType
+            {
+                Id = 4,
+                Type = "Yearly"
+            };
+
+            var user1 = new UserType
+            {
+                Id = 1,
+                Type = "Normal"
+            };
+            var user2 = new UserType
+            {
+                Id = 2,
+                Type = "Student"
+            };
+            var user3 = new UserType
+            {
+                Id = 3,
+                Type = "Senior"
+            };
+
+            var price1 = new Pricing
+            {
+                Price = 55.00,
+                TicketType = type1,
+                TicketTypeId = type1.Id,
+                UserType = user1,
+                UserTypeId = user1.Id
+            };
+            price1.Id = price1.TicketTypeId.ToString() + price1.UserTypeId.ToString();
+            var price2 = new Pricing
+            {
+                Price = 36.66,
+                TicketType = type1,
+                TicketTypeId = type1.Id,
+                UserType = user2,
+                UserTypeId = user2.Id
+            };
+            price2.Id = price2.TicketTypeId.ToString() + price2.UserTypeId.ToString();
+            var price3 = new Pricing
+            {
+                Price = 36.66,
+                TicketType = type1,
+                TicketTypeId = type1.Id,
+                UserType = user3,
+                UserTypeId = user3.Id
+            };
+            price3.Id = price3.TicketTypeId.ToString() + price3.UserTypeId.ToString();
+            var price4 = new Pricing
+            {
+                Price = 155.00,
+                TicketType = type2,
+                TicketTypeId = type2.Id,
+                UserType = user1,
+                UserTypeId = user1.Id
+            };
+            price4.Id = price4.TicketTypeId.ToString() + price4.UserTypeId.ToString();
+            var price5 = new Pricing
+            {
+                Price = 103.33,
+                TicketType = type2,
+                TicketTypeId = type2.Id,
+                UserType = user2,
+                UserTypeId = user2.Id
+            };
+            price5.Id = price5.TicketTypeId.ToString() + price5.UserTypeId.ToString();
+            var price6 = new Pricing
+            {
+                Price = 103.33,
+                TicketType = type2,
+                TicketTypeId = type2.Id,
+                UserType = user3,
+                UserTypeId = user3.Id
+            };
+            price6.Id = price6.TicketTypeId.ToString() + price6.UserTypeId.ToString();
+            var price7 = new Pricing
+            {
+                Price = 550.00,
+                TicketType = type3,
+                TicketTypeId = type3.Id,
+                UserType = user1,
+                UserTypeId = user1.Id
+            };
+            price7.Id = price7.TicketTypeId.ToString() + price7.UserTypeId.ToString();
+            var price8 = new Pricing
+            {
+                Price = 366.66,
+                TicketType = type3,
+                TicketTypeId = type3.Id,
+                UserType = user2,
+                UserTypeId = user2.Id
+            };
+            price8.Id = price8.TicketTypeId.ToString() + price8.UserTypeId.ToString();
+            var price9 = new Pricing
+            {
+                Price = 366.66,
+                TicketType = type3,
+                TicketTypeId = type3.Id,
+                UserType = user3,
+                UserTypeId = user3.Id
+            };
+            price9.Id = price9.TicketTypeId.ToString() + price9.UserTypeId.ToString();
+            var price10 = new Pricing
+            {
+                Price = 1550.00,
+                TicketType = type4,
+                TicketTypeId = type4.Id,
+                UserType = user1,
+                UserTypeId = user1.Id
+            };
+            price10.Id = price10.TicketTypeId.ToString() + price10.UserTypeId.ToString();
+            var price11 = new Pricing
+            {
+                Price = 1003.33,
+                TicketType = type4,
+                TicketTypeId = type4.Id,
+                UserType = user2,
+                UserTypeId = user2.Id
+            };
+            price11.Id = price11.TicketTypeId.ToString() + price11.UserTypeId.ToString();
+            var price12 = new Pricing
+            {
+                Price = 1003.33,
+                TicketType = type4,
+                TicketTypeId = type4.Id,
+                UserType = user3,
+                UserTypeId = user3.Id
+            };
+            price12.Id = price12.TicketTypeId.ToString() + price12.UserTypeId.ToString();
+
+            #endregion InitialElements
+
             #region TicketTypeInit
 
             if (!context.TicketTypes.Any(r => r.Type == "Hourly"))
             {
-                var type = new TicketType();
-                type.Id = 1;
-                type.Type = "Hourly";
-                context.TicketTypes.Add(type);
+                context.TicketTypes.Add(type1);
             }
 
             if (!context.TicketTypes.Any(r => r.Type == "Daily"))
             {
-                var type = new TicketType();
-                type.Id = 2;
-                type.Type = "Daily";
-                context.TicketTypes.Add(type);
+                context.TicketTypes.Add(type2);
             }
 
             if (!context.TicketTypes.Any(r => r.Type == "Monthly"))
             {
-                var type = new TicketType();
-                type.Id = 3;
-                type.Type = "Monthly";
-                context.TicketTypes.Add(type);
+                context.TicketTypes.Add(type3);
             }
 
             if (!context.TicketTypes.Any(r => r.Type == "Yearly"))
             {
-                var type = new TicketType();
-                type.Id = 4;
-                type.Type = "Yearly";
-                context.TicketTypes.Add(type);
+                context.TicketTypes.Add(type4);
             }
 
             #endregion TicketTypeInit
@@ -89,29 +227,84 @@ namespace WebApp.Migrations
 
             if (!context.UserTypes.Any(r => r.Type == "Normal"))
             {
-                var user = new UserType();
-                user.Id = 1;
-                user.Type = "Normal";
-                context.UserTypes.Add(user);
+                context.UserTypes.Add(user1);
             }
 
             if (!context.UserTypes.Any(r => r.Type == "Student"))
             {
-                var user = new UserType();
-                user.Id = 2;
-                user.Type = "Student";
-                context.UserTypes.Add(user);
+                context.UserTypes.Add(user2);
             }
 
             if (!context.UserTypes.Any(r => r.Type == "Senior"))
             {
-                var user = new UserType();
-                user.Id = 3;
-                user.Type = "Senior";
-                context.UserTypes.Add(user);
+                context.UserTypes.Add(user3);
             }
 
             #endregion UserTypeInit
+
+            #region PriceInit
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Hourly") && context.PriceList.Any(r => r.UserType.Type == "Normal")))
+            {
+                context.PriceList.Add(price1);
+            }
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Hourly") && context.PriceList.Any(r => r.UserType.Type == "Student")))
+            {
+                context.PriceList.Add(price2);
+            }
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Hourly") && context.PriceList.Any(r => r.UserType.Type == "Senior")))
+            {
+                context.PriceList.Add(price3);
+            }
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Daily") && context.PriceList.Any(r => r.UserType.Type == "Normal")))
+            {
+                context.PriceList.Add(price4);
+            }
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Daily") && context.PriceList.Any(r => r.UserType.Type == "Student")))
+            {
+                context.PriceList.Add(price5);
+            }
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Daily") && context.PriceList.Any(r => r.UserType.Type == "Senior")))
+            {
+                context.PriceList.Add(price6);
+            }
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Monthly") && context.PriceList.Any(r => r.UserType.Type == "Normal")))
+            {
+                context.PriceList.Add(price7);
+            }
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Monthly") && context.PriceList.Any(r => r.UserType.Type == "Student")))
+            {
+                context.PriceList.Add(price8);
+            }
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Monthly") && context.PriceList.Any(r => r.UserType.Type == "Senior")))
+            {
+                context.PriceList.Add(price9);
+            }
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Yearly") && context.PriceList.Any(r => r.UserType.Type == "Normal")))
+            {
+                context.PriceList.Add(price10);
+            }
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Yearly") && context.PriceList.Any(r => r.UserType.Type == "Student")))
+            {
+                context.PriceList.Add(price11);
+            }
+
+            if (!(context.PriceList.Any(r => r.TicketType.Type == "Yearly") && context.PriceList.Any(r => r.UserType.Type == "Senior")))
+            {
+                context.PriceList.Add(price12);
+            }
+
+            #endregion PriceInit
 
             var userStore = new UserStore<ApplicationUser>(context);
             var userManager = new UserManager<ApplicationUser>(userStore);
